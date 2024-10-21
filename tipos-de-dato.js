8e96b1f9f5bb3d2c077495c9b02b24b955954a53
eyael_vpg <evpadilla.333@gmail.com>
2024-10-20 20:38:49 -0600

    tipos de dato

---
+++ tipos-de-dato.js
@@ -0,0 +1,18 @@
+let edad= 5
+console.log (typeof edad)
+let saludo= "Hola, mundo"
+console.log (typeof saludo)
+let mayordeedad= true
+console.log (typeof true)
+
+//se esta pidiendo definir el tipo de variable que es 'nombre'
+//pero la variable 'nombre' no se ha asignado por tanto es de tipo 'undefined'
+console.log (typeof nombre)
+
+//la variable de tipo null es aquella a la que se le asigna el valo 'null'
+//sin embargo la consola la registra como una variable de tipo'objeto'
+let algo= null
+console.log (typeof algo)
+//para verificar que la variable 'algo' es de tipo null, se emplea el operador de
+//igualdad estricta ===, y se obtiene en respuesta 'true'
+console.log (algo === null)
