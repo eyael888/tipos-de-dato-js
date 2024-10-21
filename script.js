cdd635ebf5a0e8c68ff019b184468b20f385793e
eyael_vpg <evpadilla.333@gmail.com>
2024-10-20 21:26:54 -0600

    comparaciones y decisiones

---
+++ script.js
@@ -0,0 +1,43 @@
+let nota= 90
+if (nota >= 90){
+console.log ("Excelente")
+} else if (nota >=75 && nota<90){
+console.log ("Bien")
+} else if (nota >=60 && nota<75){
+console.log ("Suficiente")
+} else {
+console.log ("No aprobado")
+}
+
+nota= 89
+if (nota >= 90){
+    console.log ("Excelente")
+    } else if (nota >=75 && nota<90){
+    console.log ("Bien")
+    } else if (nota >=60 && nota<75){
+    console.log ("Suficiente")
+    } else {
+    console.log ("No aprobado")
+    }
+
+    nota= 74
+    if (nota >= 90){
+        console.log ("Excelente")
+        } else if (nota >=75 && nota<90){
+        console.log ("Bien")
+        } else if (nota >=60 && nota<75){
+        console.log ("Suficiente")
+        } else {
+        console.log ("No aprobado")
+        }
+
+        nota= 59
+if (nota >= 90){
+    console.log ("Excelente")
+    } else if (nota >=75 && nota<90){
+    console.log ("Bien")
+    } else if (nota >=60 && nota<75){
+    console.log ("Suficiente")
+    } else {
+    console.log ("No aprobado")
+    }
